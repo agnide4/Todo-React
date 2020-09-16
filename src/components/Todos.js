20173714b6c2ea75e302600e74fba6e1de9df78a
Samir Bello <agnide4@gmail.com>
2020-09-15 22:19:13 -0500

    Initial commit todo react app

---
+++ src/components/Todos.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+
+
+class Todos extends Component {
+
+
+    render(){
+      console.log(this.props.todos)
+
+
+
+      return (
+        <div>
+          <h1>Todos</h1>
+        </div>
+      );
+
+    }
+
+    
+
+  
+}
+
+
+export default Todos;
